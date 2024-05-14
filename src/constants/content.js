b8380949d235790c868a01445889c984e92ce1fe
Vijay-Hozo <vijay2304a@gmail.com>
2024-05-14 15:24:23 +0530

    content added

---
+++ src/constants/content.js
@@ -0,0 +1,156 @@
+import post1 from "../assets/images/post-1.png"
+import post2 from "../assets/images/post-2.png"
+import post3 from "../assets/images/post-3.png"
+import post4 from "../assets/images/post-4.png"
+import post5 from "../assets/images/post-5.png"
+import post6 from "../assets/images/post-6.png"
+import post7 from "../assets/images/post-7.png"
+import post8 from "../assets/images/post-8.png"
+import post9 from "../assets/images/post-9.png"
+import blog1 from "../assets/images/blog-1.png"
+import blog2 from "../assets/images/blog-2.png"
+import blog3 from "../assets/images/blog-3.png"
+import profile1 from "../assets/images/profile-1.png"
+import profile2 from "../assets/images/profile-2.png"
+import profile3 from "../assets/images/profile-3.png"
+
+
+export const school = [
+    {
+        "id":1,
+        "rank":"#11",
+        "description":"India’s Top-ranked private autonomous ASC colleges (2024)",
+        "color":"red"
+    },
+    {
+        "id":2,
+        "rank":"100%",
+        "description":"Placement Percentage over the Years",
+        "color": "yellow"
+    },
+    {
+        "id":3,
+        "rank":"20k+",
+        "description":"Alumni Across the Globe",
+        "color":"violet"
+    },
+    {
+        "id":4,
+        "rank":"21+",
+        "description":"Years of Education Experience",
+        "color":"yellow"
+    },
+]
+
+export const blog = [
+    {
+        "id":1,
+        "image":blog1,
+        "heading":"10 essential Study Hacks for college",
+        "desc":"Lorem ipsum dolor sit amet consectetur. Mauris arcu vulputate ipsum ac pretium est laoreet porta sed. ",
+    },
+    {
+        "id":2,
+        "image":blog2,
+        "heading":"A home away from home: 👨‍🎓 accommodation",
+        "desc":"Lorem ipsum dolor sit amet consectetur. Mauris arcu vulputate ipsum ac pretium est laoreet porta sed. ",
+    },
+    {
+        "id":3,
+        "image":blog3,
+        "heading":"Exclusive Student Discount: Best deals",
+        "desc":"Lorem ipsum dolor sit amet consectetur. Mauris arcu vulputate ipsum ac pretium est laoreet porta sed. ",
+    },
+]
+
+export const post = [
+    {
+        "id":1,
+        "image":post1,
+    },
+    {
+        "id":2,
+        "image":post2,
+    },
+    {
+        "id":3,
+        "image":post3,
+    },
+    {
+        "id":4,
+        "image":post4,
+    },
+    {
+        "id":5,
+        "image":post5,
+    },
+    {
+        "id":6,
+        "image":post6,
+    },
+    {
+        "id":7,
+        "image":post7,
+    },
+    {
+        "id":8,
+        "image":post8,
+    },
+    {
+        "id":9,
+        "image":post9,
+    },
+]
+
+export const offer = [
+    {
+        "id":1,
+        "description" : "Bachelor of Commerce with Association of Chartered Certified Accountants(9 paper exemption)",
+        "course" : ["B.Com", "ACCA"]
+    },
+    {
+        "id":2,
+        "description" : "Bachelor of Business Administration Aviation & Logistics",
+        "course" : ["BBA", "A&L"]
+    },
+    {
+        "id":3,
+        "description" : "Bachelor of Computer Application artificial intelligence & machine learning",
+        "course" : ["BCA", "AI&ML"]
+    },
+    {
+        "id":4.1,
+        "description" : "Bachelor of Computer Application artificial intelligence & machine learning",
+        "course" : ["BCA", "AI&ML"]
+    },
+    {
+        "id":3,
+        "description" : "Bachelor of Computer Application artificial intelligence & machine learning",
+        "course" : ["BCA", "AI&ML"]
+    },
+
+]
+
+export const profile = [
+    {
+        "id":1,
+        "image" : profile1,
+        "name" : "Padma Priya.S",
+        "graduation":"MBA (2020-2022)",
+        "description" : "Studying at Presidency College was truly edifying and fun. Committing to a full time course like MBA is an enriching experience by itself. On the contrary, demanded hard work, dedication and sacrifice. Nevertheless, the benefits of Presidency"
+    },
+    {
+        "id":2,
+        "image" : profile2,
+        "name" : "Padma Priya.S",
+        "graduation":"MBA (2020-2022)",
+        "description" : "Studying at Presidency College was truly edifying and fun. Committing to a full time course like MBA is an enriching experience by itself. On the contrary, demanded hard work, dedication and sacrifice. Nevertheless, the benefits of Presidency"
+    },
+    {
+        "id":3,
+        "image" : profile3,
+        "name" : "Padma Priya.S",
+        "graduation":"MBA (2020-2022)",
+        "description" : "Studying at Presidency College was truly edifying and fun. Committing to a full time course like MBA is an enriching experience by itself. On the contrary, demanded hard work, dedication and sacrifice. Nevertheless, the benefits of Presidency"
+    }
+]
